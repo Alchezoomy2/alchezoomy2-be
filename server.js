4adf7122e97b942a5ba941836e0a002627491ddc
Paul Stevens <paul.stevens.developer@gmail.com>
2021-01-10 15:28:12 -0800

    beginss backend refactor

--- server.js
+++ server.js
@@ -5,16 +5,11 @@ const fs = require('fs');
 const app = require('./lib/app');
 const path = require('path');
 
-const PORT = process.env.PORT || 7890;
-
 const sslServer = https.createServer({
   key: fs.readFileSync(path.join(__dirname, '/cert/', 'privkey.pem')),
   cert: fs.readFileSync(path.join(__dirname, '/cert/', 'cert.pem'))
 }, app)
 
 
-// app.listen(PORT, () => {
-//   console.log(`insecure server on ${PORT}`);
-// });
 
 sslServer.listen(3443, () => console.log('secure server on port 3443'))
