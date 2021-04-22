b7871422c0157914fbce28f5937bfde1164536fa
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-04-21 21:27:59 -0700

    testing ssl

--- server.js
+++ server.js
@@ -11,9 +11,4 @@ const sslServer = https.createServer({
   cert: fs.readFileSync(path.join(__dirname, '../../../etc/letsencrypt/live/api.alchemylectures.com', 'cert.pem'))
 }, app)
 
-const cert = fs.readFileSync(path.join(__dirname, '../../../etc/letsencrypt/live/api.alchemylectures.com', 'cert.pem'));
-console.log(JSON.parse(cert))
-
-
-console.log(sslServer)
 sslServer.listen(3443, () => console.log('secure server on port 3443'))
