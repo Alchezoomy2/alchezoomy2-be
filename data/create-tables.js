5be8c71407168c7d0f04af08a7c453bbcd58d364
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-05-12 11:18:25 -0700

    adds timestamp to transcript db

--- data/create-tables.js
+++ data/create-tables.js
@@ -54,6 +54,7 @@ async function run() {
                     teacher_id INTEGER NOT NULL,
                     identifier VARCHAR(512) NOT NULL,
                     time_start FLOAT NOT NULL,
+                    timestamp TEXT NOT NULL,
                     time_end FLOAT NOT NULL,
                     text VARCHAR NOT NULL
                     );
