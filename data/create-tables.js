5c254b162c8ba7277451606e2317724d1675c765
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-05-11 21:20:42 -0700

    undoundoundo

--- data/create-tables.js
+++ data/create-tables.js
@@ -54,7 +54,6 @@ async function run() {
                     teacher_id INTEGER NOT NULL,
                     identifier VARCHAR(512) NOT NULL,
                     time_start FLOAT NOT NULL,
-                    parsed_timestamp INTEGER NOT NULL,
                     time_end FLOAT NOT NULL,
                     text VARCHAR NOT NULL
                     );
