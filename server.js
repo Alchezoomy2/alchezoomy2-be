b8c0c46034ce9da86c4fd6b7f5192a6ccb4bb893
Paul Stevens <paul.stevens.developer@gmail.com>
2021-06-15 16:39:34 -0700

    update SSL certificates

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const path = require('path');
 
 
 const sslServer = https.createServer({
-  key: fs.readFileSync(path.join(__dirname, '../../../etc/letsencrypt/live/api.alchemylectures.com', 'privkey.pem')),
-  cert: fs.readFileSync(path.join(__dirname, '../../../etc/letsencrypt/live/api.alchemylectures.com', 'cert.pem'))
+  key: fs.readFileSync(path.join(__dirname, '../../../etc/letsencrypt/live/app.alchemylectures.com', 'privkey.pem')),
+  cert: fs.readFileSync(path.join(__dirname, '../../../etc/letsencrypt/live/app.alchemylectures.com', 'cert.pem'))
 }, app)
 
 sslServer.listen(3443, () => console.log('secure server on port 3443'))
